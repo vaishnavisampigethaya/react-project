5283f20fcdfba7c34ddeb31ebd83812960bc9e21
vaishnavisampigethya <vaishnavisampigethya@gmail.com>
2021-05-15 18:08:51 +0530

    second commit'

---
+++ myfirst/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="..\IMG-20190928-WA0027.jpg" className="App-logo" alt="vaishu" />
+        <p>
+        <code>Friends Forever</code> 
+        </p>
+        <h1>My First Project. Good Job VAISHNAVI</h1>
+         
+      </header>
+    </div>
+  );
+}
+
+export default App;
